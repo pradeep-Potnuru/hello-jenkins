1adf7b858d19af862fa023af06661d21e72800ea
pradeep <pradeep.cse2@gmail.com>
2016-08-01 10:34:03 +0530

    reports related things

--- karma.conf.js
+++ karma.conf.js
@@ -38,8 +38,11 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['dots','junit'],
 
+    junitReporter: {
+      outputFile : 'test-results.xml'
+    },
 
     // web server port
     port: 9876,
